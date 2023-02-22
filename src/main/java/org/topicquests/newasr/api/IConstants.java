88d07d2121f6d1bc49ae3cf8b881e4bbd9f2ec1e
Jack Park <jackpark@gmail.com>
2023-02-22 15:58:54 -0800

    revised fields and keys for new schema

--- src/main/java/org/topicquests/newasr/api/IConstants.java
+++ src/main/java/org/topicquests/newasr/api/IConstants.java
@@ -8,14 +8,23 @@ package org.topicquests.newasr.api;
  *
  */
 public interface IConstants {
-	//for Wordgrams
+	//for Wordgrams asr-schema - fields
 	public static final String
 		ID_FIELD		= "id",
-		LOX_FIELD		= "lox", // locators
-		IN_FIELD		= "inLinks",
-		OUT_FIELD		= "outLinks",
-		DBPED_FIELD		= "dbp",
-		WIKID_FIELD		= "wikd",
-		POS_FIELD		= "pos",
-		WORDS_FIELD		= "words";
+		KEY_FIELD		= "_key",
+		VALUE_FIELD		= "_val";
+	
+	// standard keys
+	public static final String
+		LOX_KEY			= "lox", 	// locators
+		IN_KEY			= "inLinks", // {sentenceId, gramId}
+		OUT_KEY			= "outLinks", // {sentenceId, gramId}
+		DBPED_KEY		= "dbp",	// dbpedia
+		WIKID_KEY		= "wikd",	// wikidata
+		POS_KEY			= "pos",	// part of speech
+		WORDS_KEY		= "words",	// the text for this gram
+		INVERSE_KEY		= "inverse",// inverse predicate - only for passive predicates
+		CANNON_KEY		= "cannon",	// canonical NER
+		SYNONMY_KEY		= "synon",	// synonyms
+		ANTONYM_KEY		= "anton";	// antonyms
 }
