21d26e624988ee52a143417790ca17f34d211f88
Jack Park <jackpark@gmail.com>
2023-02-27 14:35:15 -0800

    Added First dictionary test

---
+++ src/test/java/org/topicquests/newasr/test/FirstNodeTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.topicquests.newasr.test;
+
+import org.topicquests.newasr.api.IWordGram;
+import org.topicquests.newasr.impl.WordGram;
+import org.topicquests.support.api.IResult;
+
+/**
+ * @author jackpark
+ *
+ */
+public class FirstNodeTest extends TestingRoot {
+	public static final String
+		TERM	= "Foo Bar",
+		POS		= "noun";
+	/**
+	 * 
+	 */
+	public FirstNodeTest() {
+		super();
+		
+		//IWordGram wg = new WordGram();
+		IResult r = model.processTerm(TERM, POS);
+		System.out.println("A "+r.getErrorString());
+		System.out.println("B "+r.getResultObject());
+		
+		environment.shutDown();
+		System.exit(0);
+	}
+
+}
