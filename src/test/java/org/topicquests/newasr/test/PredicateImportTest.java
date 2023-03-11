dddbb4efa49abe4d144a05486af0bc871a2f9916
Jack Park <jackpark@gmail.com>
2023-03-11 14:30:41 -0800

    adding testing for csv predicate import

---
+++ src/test/java/org/topicquests/newasr/test/PredicateImportTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.topicquests.newasr.test;
+
+import org.topicquests.newasr.bootstrap.PredicateImporter;
+import org.topicquests.support.api.IResult;
+
+/**
+ * @author jackpark
+ *
+ */
+public class PredicateImportTest extends TestingRoot {
+	private PredicateImporter predImporter;
+
+	/**
+	 * 
+	 */
+	public PredicateImportTest() {
+		super();
+
+		predImporter = environment.getPredicateImporter();
+		IResult r = predImporter.bootPredicates();
+		System.out.println("A "+r.getErrorString());
+		
+		environment.shutDown();
+		System.exit(0);
+	}
+
+}
