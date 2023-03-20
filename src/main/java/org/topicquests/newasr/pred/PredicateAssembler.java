e347af3c845edcf36a2c3e398a015a8314afafa6
Jack Park <jackpark@gmail.com>
2023-03-20 16:50:10 -0700

    Starting predicate handling

---
+++ src/main/java/org/topicquests/newasr/pred/PredicateAssembler.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.topicquests.newasr.pred;
+
+import org.topicquests.newasr.ASREnvironment;
+import org.topicquests.support.ResultPojo;
+import org.topicquests.support.api.IResult;
+
+import com.google.gson.JsonObject;
+
+/**
+ * @author jackpark
+ *
+ */
+public class PredicateAssembler {
+	private ASREnvironment environment;
+
+	/**
+	 * 
+	 */
+	public PredicateAssembler(ASREnvironment e) {
+		environment = e;
+	}
+
+	public IResult processSentencePredicates(long sentenceId, JsonObject predicates) {
+		IResult result = new ResultPojo();
+		// TODO Auto-generated method stub
+		return result;
+	}
+}
